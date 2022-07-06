fa1a53a66277da416f071f3acffbe46eb0206243
Ryan Large <101679941+RyanLarge13@users.noreply.github.com>
2022-07-06 00:53:39 -0700

    Updating script.js with an added array constant variable containing multiple objects and their properties
    adding in a variable containing objects within an array of each pokemon character I chose and their details

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,34 @@
-alert('Hello World!!!!');
+// for lesson 1.1 if by chance you do not see this code before my new commits for the next lesson appear in lesson 1.2 
 
-const favoriteFood = 'Pizza, or maybe burritos.'
+// alert('Hello World!!!!');
 
-document.write(favoriteFood);
+// const favoriteFood = 'Pizza, or maybe burritos.'
+
+// document.write(favoriteFood);
+
+const pokemonList = [
+    {name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+    },
+    {name: 'Charizard',
+    height: 1.7,
+    types: ['fire', 'flying']
+    },
+    {name: 'Arcinine',
+    height: 1.9,
+    types: ['fire']
+    },
+    {name: 'Alakazam',
+    height: 1.5,
+    types: ['psychic']
+    },
+    {name: 'Zapdos',
+    height: 1.6,
+    types: ['electric', 'flying']
+    },
+    {name: 'Kyogre',
+    height: 4.5,
+    types: ['water']
+    },
+]
