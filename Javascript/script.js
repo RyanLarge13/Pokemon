930cf686f435845bbb23e2fddcf187ad3d19a133
Ryan Large <101679941+RyanLarge13@users.noreply.github.com>
2022-07-07 02:23:06 -0700

    Completing lesson 1.6 adding in event listeners and manipulating the DOM
    Adding in styling, and basic html tags for the creation of my Pokémon app. Including functions to create elements and append them to the DOM.
    
    Also adding an event listener to each button that will listen for a click and run a function that console.log()'s the Pokémon objects key value pairs.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,6 @@
+//IIFE function
 const pokemonList = (() => {
+    //list of pokemon
     let pokemon = [
         {
             name: 'Bulbasaur',
@@ -32,6 +34,7 @@ const pokemonList = (() => {
         },
     ];
 
+    //begin functions for handling dom minipulation and pokemon objects
     const add = (obj) => {
         let keys1 = Object.keys(pokemon[0]);
         let keys2 = Object.keys(obj);
@@ -41,32 +44,57 @@ const pokemonList = (() => {
         } else {
         pokemon.push(obj);
         }
-    }
+    };
 
     const getAll = () => {
         return pokemon;
-    }
+    };
 
+    //for future search bar at top of page
     const filter = (string) => {
         pokemon.filter(name => {
             if (name.name === string) {
                 console.log(name)
             }
-        })
+        });
+    };
+
+    //creating the list of cards in the dom and buttons to go with them
+    const addListItem = (pokemon) => {
+        let cardContainer = document.querySelector('.card-container');
+        let card = document.createElement('div');
+        card.classList.add('card');
+        let cardBtn = document.createElement('button');
+        cardBtn.innerText = `${pokemon.name}`;
+        cardBtn.classList.add('btn');
+        cardContainer.appendChild(card);
+        card.appendChild(cardBtn);
+
+        btnEvent(pokemon, cardBtn);
     }
+
+    //adding an eventlistener to each button and when fired calling showDetails();
+    const btnEvent = (pokemon, cardBtn) => {
+        cardBtn.addEventListener('click', () => {
+            showDetails(pokemon);
+        });
+    };
+
+    const showDetails = (pokemon) => {
+        console.log(pokemon);
+    };
 
     return {
         add: add,
         getAll: getAll,
         filter: filter,
-    }
+        addListItem: addListItem,
+        showDetails: showDetails,
+    };
 })();
 
+//forEach loop calling the addListItem function and initiating dynamic dom elemetns for pokemon
 pokemonList.getAll().forEach((obj) => {
-    const { name, height } = obj;
-    let str = `${name} (height: ${height})`;
-    if (height > 4.0) {
-        str += ' - Wow! That is big!!';
-    }
-    document.write(`<p>${str}</p><br>`)
+    pokemonList.addListItem(obj)
 });
+
